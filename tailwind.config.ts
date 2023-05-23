0b7703868301de5ed434bbd485f902022230b744
王建明 <jianming.wang@dilu.com>
2023-05-23 10:57:46 +0800

    style: 修复深色主题样式

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss';
 import plugin from './src/plugin';
 
 export default {
+  darkMode: ['class', '[data-prefers-color="dark"]'],
   content: ['./src/components/**/*.{ts,tsx,js,jsx}', './.dumi/**/*.{ts,tsx,js,jsx}'],
   theme: {
     extend: {},
