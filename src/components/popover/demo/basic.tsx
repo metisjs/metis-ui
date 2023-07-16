a5d51720c451f14cbd46e6a3221684e42d4647c3
王建明 <wangjianming.x@gmail.com>
2023-07-16 22:43:42 +0800

    feat: popover

---
+++ src/components/popover/demo/basic.tsx
@@ -0,0 +1,20 @@
+/**
+ * description: 最简单的用法，浮层的大小由内容区域决定。
+ */
+import { Button, Popover } from 'meta-ui';
+import React from 'react';
+
+const content = (
+  <div>
+    <p>Content</p>
+    <p>Content</p>
+  </div>
+);
+
+const App: React.FC = () => (
+  <Popover content={content} title="Title">
+    <Button type="primary">Hover me</Button>
+  </Popover>
+);
+
+export default App;
---
+++ src/components/popover/index.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import { clsx, getComplexCls } from '../_util/classNameUtils';
+import type { RenderFunction } from '../_util/getRenderPropValue';
+import { getRenderPropValue } from '../_util/getRenderPropValue';
+import { ConfigContext } from '../config-provider';
+import type { AbstractTooltipProps, TooltipRef } from '../tooltip';
+import Tooltip from '../tooltip';
+
+export interface PopoverProps extends AbstractTooltipProps {
+  title?: React.ReactNode | RenderFunction;
+  content?: React.ReactNode | RenderFunction;
+}
+
+interface OverlayProps {
+  prefixCls?: string;
+  title?: PopoverProps['title'];
+  content?: PopoverProps['content'];
+}
+
+const Overlay: React.FC<OverlayProps> = ({ title, content, prefixCls }) => (
+  <>
+    {title && (
+      <div className={clsx(`${prefixCls}-title`, 'mb-2 box-border min-w-[177px] font-semibold')}>
+        {getRenderPropValue(title)}
+      </div>
+    )}
+    <div className={`${prefixCls}-inner-content`}>{getRenderPropValue(content)}</div>
+  </>
+);
+
+const Popover = React.forwardRef<TooltipRef, PopoverProps>((props, ref) => {
+  const {
+    title,
+    content,
+    className,
+    placement = 'top',
+    trigger = 'hover',
+    mouseEnterDelay = 0.1,
+    mouseLeaveDelay = 0.1,
+    overlayStyle = {},
+    ...otherProps
+  } = props;
+  const { getPrefixCls } = React.useContext(ConfigContext);
+
+  const complexCls = getComplexCls(className);
+
+  const prefixCls = getPrefixCls('popover');
+
+  const overlayCls = clsx(
+    'visible absolute z-[1070] box-border block w-max max-w-[250px] origin-[var(--arrow-x,50%)_var(--arrow-y,50%)] [--meta-arrow-background-color:hsla(var(--neutral-bg-elevated))]',
+    complexCls.overlay,
+  );
+
+  const popupInnerCls = clsx(
+    'z-[1030] cursor-auto select-text rounded-lg p-3 text-neutral-text shadow-lg ring-1 ring-neutral-border-secondary focus:outline-none',
+  );
+
+  return (
+    <Tooltip
+      placement={placement}
+      trigger={trigger}
+      mouseEnterDelay={mouseEnterDelay}
+      mouseLeaveDelay={mouseLeaveDelay}
+      overlayStyle={overlayStyle}
+      {...otherProps}
+      prefixCls={prefixCls}
+      className={{ overlay: overlayCls, popupInner: popupInnerCls }}
+      ref={ref}
+      overlay={
+        title || content ? <Overlay prefixCls={prefixCls} title={title} content={content} /> : null
+      }
+      transition={{
+        enter: 'transition duration-[200ms]',
+        enterFrom: 'opacity-0 scale-0',
+        enterTo: 'opacity-100 scale-100',
+        leave: 'transition duration-[200ms]',
+        leaveFrom: 'opacity-100 scale-100 ',
+        leaveTo: 'opacity-0 scale-0',
+      }}
+    />
+  );
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  Popover.displayName = 'Popover';
+}
+
+export default Popover;
