732073cc15a4f8ddb90a62240b9fdc2fb1441d9b
王建明 <jianming.wang@dilu.com>
2023-08-01 17:51:00 +0800

    fix: wip transition bug

---
+++ src/components/transition/hooks/useDomMotionEvents.ts
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { useRef } from 'react';
+
+export default (
+  callback: (event: TransitionEvent) => void,
+): [(element: HTMLElement) => void, (element: HTMLElement) => void] => {
+  const cacheElementRef = useRef<HTMLElement>();
+
+  // Cache callback
+  const callbackRef = useRef(callback);
+  callbackRef.current = callback;
+
+  // Internal transition event handler
+  const onInternalTransitionEnd = React.useCallback((event: TransitionEvent) => {
+    callbackRef.current(event);
+  }, []);
+
+  // Remove events
+  function removeTransitionEvents(element?: HTMLElement) {
+    if (element) {
+      element.removeEventListener('transitionend', onInternalTransitionEnd);
+    }
+  }
+
+  // Patch events
+  function patchTransitionEvents(element: HTMLElement) {
+    if (cacheElementRef.current && cacheElementRef.current !== element) {
+      removeTransitionEvents(cacheElementRef.current);
+    }
+
+    if (element && element !== cacheElementRef.current) {
+      element.addEventListener('transitionend', onInternalTransitionEnd);
+
+      // Save as cache in case dom removed trigger by `deadline`
+      cacheElementRef.current = element;
+    }
+  }
+
+  // Clean up when removed
+  React.useEffect(
+    () => () => {
+      removeTransitionEvents(cacheElementRef.current);
+    },
+    [],
+  );
+
+  return [patchTransitionEvents, removeTransitionEvents];
+};
