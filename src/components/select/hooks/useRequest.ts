a7c1e0df9072e86baf3f75628a0203c82bf9e2b0
王建明 <jianming.wang@dilu.com>
2024-06-19 16:56:49 +0800

    feat(select): with request doing

--- src/components/select/hooks/useRequest.ts
+++ src/components/select/hooks/useRequest.ts
@@ -1,12 +1,15 @@
 import { useRequest, useUpdateEffect } from 'ahooks';
+import { useMemo, useState } from 'react';
 import { BaseOptionType, RequestConfig } from '../interface';
+
+const PAGE_SIZE = 30;
 
 export default function <TData extends BaseOptionType>(
   request?: RequestConfig<TData, any[]>,
   showSearch?: boolean,
   searchValue?: string,
   optionFilterProp?: string,
-  // pagination?: boolean,
+  pagination?: boolean,
 ): [TData[], boolean] {
   let requestService: any = () => {};
   let requestOptions = undefined;
@@ -17,23 +20,38 @@ export default function <TData extends BaseOptionType>(
     requestOptions = request.options;
   }
 
-  const { defaultParams: rawDefaultParams = [], ...restOptions } = requestOptions ?? {};
-  let defaultParams = [...rawDefaultParams];
-  if (showSearch) {
-    defaultParams.unshift({
-      filters: { [optionFilterProp ?? 'keyword']: searchValue?.trim() || undefined },
-    });
-  }
+  const { defaultParams = [], ...restOptions } = requestOptions ?? {};
+
+  const [current] = useState(1);
+
+  const params = useMemo(() => {
+    let firstParam: Record<string, any> | undefined = undefined;
+    if (showSearch) {
+      firstParam = {
+        filters: { [optionFilterProp ?? 'keyword']: searchValue?.trim() || undefined },
+      };
+    }
+
+    if (pagination) {
+      firstParam = {
+        ...firstParam,
+        current,
+        pageSize: PAGE_SIZE,
+      };
+    }
+
+    return [firstParam, ...defaultParams].filter(Boolean);
+  }, [searchValue, current]);
 
   const { data, loading, runAsync } = useRequest(requestService, {
-    defaultParams,
+    defaultParams: params,
     ...restOptions,
   });
   const { data: options = [] } = data || {};
 
   useUpdateEffect(() => {
-    runAsync(...defaultParams);
-  }, [searchValue]);
+    runAsync(...params);
+  }, [params]);
 
   return [options, loading];
 }
