cca04c9faa72c5f6d3b18c0f1f89047a95a831f3
王建明 <jianming.wang@dilu.com>
2024-06-26 17:28:23 +0800

    style(pagination): 样式优化

--- src/components/pagination/Pager.tsx
+++ src/components/pagination/Pager.tsx
@@ -41,7 +41,7 @@ const Pager: React.FC<PagerProps> = (props) => {
     },
     'min-w-9 cursor-pointer rounded-md px-2 text-center leading-9',
     {
-      'bg-primary-bg font-medium text-primary': active,
+      'bg-primary-bg font-medium text-primary shadow-sm': active,
       'hover:bg-neutral-fill-tertiary': !active,
       'min-w-8 leading-8': isSmall,
     },
