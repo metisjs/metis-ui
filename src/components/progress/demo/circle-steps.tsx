f3d4a43b1c33e2f545b44678a24fd2fc34ebe63c
王建明 <jianming.wang@dilu.com>
2024-07-02 17:22:44 +0800

    docs(progress): demo update

--- src/components/progress/demo/circle-steps.tsx
+++ src/components/progress/demo/circle-steps.tsx
@@ -1,19 +1,19 @@
-import { Progress, Space } from 'metis-ui';
+import { Progress, Slider, Space } from 'metis-ui';
 import React from 'react';
 
 const App: React.FC = () => {
-  const [stepsCount] = React.useState<number>(5);
-  const [stepsGap] = React.useState<number>(7);
+  const [stepsCount, setStepsCount] = React.useState<number>(5);
+  const [stepsGap, setStepsGap] = React.useState<number>(7);
   return (
     <>
       <h5>Custom count:</h5>
-      {/* <Slider min={2} max={10} value={stepsCount} onChange={setStepsCount} /> */}
+      <Slider min={2} max={10} value={stepsCount} onChange={setStepsCount} />
       <h5>Custom gap:</h5>
-      {/* <Slider step={4} min={0} max={40} value={stepsGap} onChange={setStepsGap} /> */}
+      <Slider step={4} min={0} max={40} value={stepsGap} onChange={setStepsGap} />
       <Space wrap block className="mt-4">
         <Progress
           type="dashboard"
-          steps={8}
+          steps={{ count: stepsCount, gap: stepsGap }}
           percent={50}
           trailColor="rgba(0, 0, 0, 0.06)"
           strokeWidth={20}
