8a430da7aa341acb544f89d573614fc1045f1ec1
王建明 <jianming.wang@dilu.com>
2024-08-15 17:47:46 +0800

    feat: color picker coding

--- src/components/color-picker/components/Slider.tsx
+++ src/components/color-picker/components/Slider.tsx
@@ -1,29 +1,169 @@
-import type { FC } from 'react';
-import React, { useRef } from 'react';
-import useColorDrag from '../hooks/useColorDrag';
-import type { HSBAColorType, TransformOffset } from '../interface';
-import Palette from './Palette';
-
 import classNames from 'classnames';
 import { useEvent } from 'rc-util';
+import type { FC } from 'react';
+import React, { useRef } from 'react';
 import { Color } from '../color';
+import useColorDrag from '../hooks/useColorDrag';
+import type { HSBAColorType, TransformOffset } from '../interface';
 import { calcOffset, calculateColor } from '../util';
 import Gradient from './Gradient';
 import Handler from './Handler';
+import Palette from './Palette';
 import Transform from './Transform';
+import {GetContextProp} from '../../_util/type'
 
-export interface BaseSliderProps {
+export interface GradientColorSliderProps {
   prefixCls: string;
   colors: { percent: number; color: string }[];
   min: number;
   max: number;
-  value: number;
   disabled: boolean;
-  onChange: (value: number) => void;
-  onChangeComplete: (value: number) => void;
-  type: HSBAColorType;
   color: Color;
+  value: number[];
+  onChange?: (value: number[]) => void;
+  onChangeComplete: (value: number[]) => void;
+  range?: boolean;
+  className?: string;
+  activeIndex?: number;
+  onActive?: (index: number) => void;
+  type: HSBAColorType | 'gradient';
+
+  // Drag events
+  onDragStart?: GetContextProp<typeof UnstableContext, 'onDragStart'>;
+  onDragChange?: GetContextProp<typeof UnstableContext, 'onDragChange'>;
+
+  // Key event
+  onKeyDelete?: (index: number) => void;
 }
+
+export const GradientColorSlider = (props: GradientColorSliderProps) => {
+  const {
+    prefixCls,
+    colors,
+    type,
+    color,
+    range = false,
+    className,
+    activeIndex,
+    onActive,
+
+    onDragStart,
+    onDragChange,
+    onKeyDelete,
+
+    ...restProps
+  } = props;
+
+  const sliderProps = {
+    ...restProps,
+    track: false,
+  };
+
+  // ========================== Background ==========================
+  const linearCss = React.useMemo(() => {
+    const colorsStr = colors.map((c) => `${c.color} ${c.percent}%`).join(', ');
+    return `linear-gradient(90deg, ${colorsStr})`;
+  }, [colors]);
+
+  const pointColor = React.useMemo(() => {
+    if (!color || !type) {
+      return null;
+    }
+
+    if (type === 'alpha') {
+      return color.toRgbString();
+    }
+
+    return `hsl(${color.toHsb().h}, 100%, 50%)`;
+  }, [color, type]);
+
+  // ======================= Context: Slider ========================
+  const onInternalDragStart: GetContextProp<typeof UnstableContext, 'onDragStart'> = useEvent(
+    onDragStart!,
+  );
+
+  const onInternalDragChange: GetContextProp<typeof UnstableContext, 'onDragChange'> = useEvent(
+    onDragChange!,
+  );
+
+  const unstableContext = React.useMemo(
+    () => ({
+      onDragStart: onInternalDragStart,
+      onDragChange: onInternalDragChange,
+    }),
+    [],
+  );
+
+  // ======================= Context: Render ========================
+  const handleRender: GetProp<SliderInternalContextProps, 'handleRender'> = useEvent(
+    (ori, info) => {
+      const { onFocus, style, className: handleCls, onKeyDown } = ori.props;
+
+      // Point Color
+      const mergedStyle = { ...style };
+      if (type === 'gradient') {
+        mergedStyle.background = getGradientPercentColor(colors, info.value);
+      }
+
+      return React.cloneElement(ori, {
+        onFocus: (e: React.FocusEvent<HTMLDivElement>) => {
+          onActive?.(info.index);
+          onFocus?.(e);
+        },
+        style: mergedStyle,
+        className: classNames(handleCls, {
+          [`${prefixCls}-slider-handle-active`]: activeIndex === info.index,
+        }),
+        onKeyDown: (e: React.KeyboardEvent<HTMLDivElement>) => {
+          if ((e.key === 'Delete' || e.key === 'Backspace') && onKeyDelete) {
+            onKeyDelete(info.index);
+          }
+
+          onKeyDown?.(e);
+        },
+      });
+    },
+  );
+
+  const sliderContext: SliderInternalContextProps = React.useMemo(
+    () => ({
+      direction: 'ltr',
+      handleRender,
+    }),
+    [],
+  );
+
+  // ============================ Render ============================
+  return (
+    <SliderInternalContext.Provider value={sliderContext}>
+      <UnstableContext.Provider value={unstableContext}>
+        <Slider
+          {...sliderProps}
+          className={classNames(className, `${prefixCls}-slider`)}
+          tooltip={{ open: false }}
+          range={{
+            editable: range,
+            minCount: 2,
+          }}
+          styles={{
+            rail: {
+              background: linearCss,
+            },
+            handle: pointColor
+              ? {
+                  background: pointColor,
+                }
+              : {},
+          }}
+          classNames={{
+            rail: `${prefixCls}-slider-rail`,
+            handle: `${prefixCls}-slider-handle`,
+          }}
+        />
+      </UnstableContext.Provider>
+    </SliderInternalContext.Provider>
+  );
+};
 
 const Slider: FC<BaseSliderProps> = (props) => {
   const { prefixCls, colors, disabled, onChange, onChangeComplete, color, type } = props;
