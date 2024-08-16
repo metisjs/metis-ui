db899fbfeec43b617e282bc2eddc344716d969e5
王建明 <jianming.wang@dilu.com>
2024-08-16 11:43:19 +0800

    feat: color picker coding

---
+++ src/components/color-picker/aColor.ts
@@ -0,0 +1,159 @@
+import { ColorInput, HSV, TinyColor } from '@ctrl/tinycolor';
+import type { ColorGenInput, Colors, HSB } from './interface';
+import { getRoundNumber } from './util';
+
+const convertHsb2Hsv = (color?: ColorGenInput): ColorInput => {
+  if (color instanceof TinyColor) {
+    return color;
+  }
+
+  if (color && typeof color === 'object' && 'h' in color && 'b' in color) {
+    const { b, ...resets } = color as HSB;
+    return {
+      ...resets,
+      v: b,
+    } as HSV;
+  }
+  if (typeof color === 'string' && /hsb/.test(color)) {
+    return color.replace(/hsb/, 'hsv');
+  }
+  return color as ColorInput;
+};
+
+export class Color extends TinyColor {
+  constructor(color?: ColorGenInput) {
+    super(convertHsb2Hsv(color));
+  }
+
+  toHsbString() {
+    const hsb = this.toHsb();
+    const saturation = getRoundNumber(hsb.s * 100);
+    const lightness = getRoundNumber(hsb.b * 100);
+    const hue = getRoundNumber(hsb.h);
+    const alpha = hsb.a;
+    const hsbString = `hsb(${hue}, ${saturation}%, ${lightness}%)`;
+    const hsbaString = `hsba(${hue}, ${saturation}%, ${lightness}%, ${alpha.toFixed(
+      alpha === 0 ? 0 : 2,
+    )})`;
+    return alpha === 1 ? hsbString : hsbaString;
+  }
+
+  toHsb() {
+    const { v, ...resets } = this.toHsv();
+    return {
+      ...resets,
+      b: v,
+      a: this.a,
+    };
+  }
+
+  clone(): Color {
+    return new Color(this.toString());
+  }
+}
+
+export type GradientColor = {
+  color: AggregationColor;
+  percent: number;
+}[];
+
+export class AggregationColor {
+  /** Original Color object */
+  private metaColor: Color;
+
+  private colors: GradientColor | undefined;
+
+  public cleared = false;
+
+  constructor(color: ColorGenInput<AggregationColor> | Colors<AggregationColor>) {
+    // Clone from another AggregationColor
+    if (color instanceof AggregationColor) {
+      this.metaColor = color.metaColor.clone();
+      this.colors = color.colors?.map((info) => ({
+        color: new AggregationColor(info.color),
+        percent: info.percent,
+      }));
+      this.cleared = color.cleared;
+      return;
+    }
+
+    const isArray = Array.isArray(color);
+
+    if (isArray && color.length) {
+      this.colors = color.map(({ color: c, percent }) => ({
+        color: new AggregationColor(c),
+        percent,
+      }));
+      this.metaColor = new Color(this.colors[0].color.metaColor);
+    } else {
+      this.metaColor = new Color(isArray ? '' : color);
+    }
+
+    if (!color || (isArray && !this.colors)) {
+      this.metaColor.setAlpha(0);
+      this.cleared = true;
+    }
+  }
+
+  toHsb() {
+    return this.metaColor.toHsb();
+  }
+
+  toHsbString() {
+    return this.metaColor.toHsbString();
+  }
+
+  toHex() {
+    return this.metaColor.toHex();
+  }
+
+  toHexString() {
+    return this.metaColor.toHexString();
+  }
+
+  toRgb() {
+    return this.metaColor.toRgb();
+  }
+
+  toRgbString() {
+    return this.metaColor.toRgbString();
+  }
+
+  isGradient(): boolean {
+    return !!this.colors && !this.cleared;
+  }
+
+  getColors(): GradientColor {
+    return this.colors || [{ color: this, percent: 0 }];
+  }
+
+  toCssString(): string {
+    const { colors } = this;
+
+    // CSS line-gradient
+    if (colors) {
+      const colorsStr = colors.map((c) => `${c.color.toRgbString()} ${c.percent}%`).join(', ');
+      return `linear-gradient(90deg, ${colorsStr})`;
+    }
+
+    return this.metaColor.toRgbString();
+  }
+
+  equals(color: AggregationColor | null): boolean {
+    if (!color || this.isGradient() !== color.isGradient()) {
+      return false;
+    }
+
+    if (!this.isGradient()) {
+      return this.toHexString() === color.toHexString();
+    }
+
+    return (
+      this.colors!.length === color.colors!.length &&
+      this.colors!.every((c, i) => {
+        const target = color.colors![i];
+        return c.percent === target.percent && c.color.equals(target.color);
+      })
+    );
+  }
+}
