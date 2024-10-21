1d507d9bc7af02b5167f73e475e8716d5ba8db96
王建明 <jianming.wang@dilu.com>
2024-10-21 10:45:12 +0800

    feat(tooltip): support preset color

--- src/components/tooltip/demo/colorful.tsx
+++ src/components/tooltip/demo/colorful.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { Button, Space, Tooltip } from 'metis-ui';
+import { Button, Divider, Space, Tooltip } from 'metis-ui';
+
+const colors = [
+  'blue',
+  'purple',
+  'cyan',
+  'green',
+  'pink',
+  'red',
+  'orange',
+  'yellow',
+  'lime',
+  'amber',
+  'emerald',
+  'teal',
+  'sky',
+  'indigo',
+];
 
 const customColors = ['#f50', '#2db7f5', '#87d068', '#108ee9'];
 
 const App: React.FC = () => (
   <>
+    <Divider orientation="left">Presets</Divider>
+    <Space wrap>
+      {colors.map((color) => (
+        <Tooltip title="prompt text" color={color} key={color}>
+          <Button>{color}</Button>
+        </Tooltip>
+      ))}
+    </Space>
+    <Divider orientation="left">Custom</Divider>
     <Space wrap>
       {customColors.map((color) => (
         <Tooltip title="prompt text" color={color} key={color}>
