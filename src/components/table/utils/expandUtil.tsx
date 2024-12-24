b00f92aa8187e0b321ca343d73b96d8d0074ef23
王建明 <jianming.wang@dilu.com>
2024-12-24 16:23:52 +0800

    feat(table): wip

---
+++ src/components/table/utils/expandUtil.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import classNames from 'classnames';
+import type { RenderExpandIconProps, Key, GetRowKey, ExpandableConfig } from '../interface';
+
+export function renderExpandIcon<RecordType>({
+  prefixCls,
+  record,
+  onExpand,
+  expanded,
+  expandable,
+}: RenderExpandIconProps<RecordType>) {
+  const expandClassName = `${prefixCls}-row-expand-icon`;
+
+  if (!expandable) {
+    return <span className={classNames(expandClassName, `${prefixCls}-row-spaced`)} />;
+  }
+
+  const onClick: React.MouseEventHandler<HTMLElement> = event => {
+    onExpand(record, event);
+    event.stopPropagation();
+  };
+
+  return (
+    <span
+      className={classNames(expandClassName, {
+        [`${prefixCls}-row-expanded`]: expanded,
+        [`${prefixCls}-row-collapsed`]: !expanded,
+      })}
+      onClick={onClick}
+    />
+  );
+}
+
+export function findAllChildrenKeys<RecordType>(
+  data: readonly RecordType[],
+  getRowKey: GetRowKey<RecordType>,
+  childrenColumnName: string,
+): Key[] {
+  const keys: Key[] = [];
+
+  function dig(list: readonly RecordType[]) {
+    (list || []).forEach((item, index) => {
+      keys.push(getRowKey(item, index));
+
+      dig((item as any)[childrenColumnName]);
+    });
+  }
+
+  dig(data);
+
+  return keys;
+}
+
+export function computedExpandedClassName<RecordType>(
+  cls: ExpandableConfig<RecordType>['expandedRowClassName'],
+  record: RecordType,
+  index: number,
+  indent: number,
+) {
+  if (typeof cls === 'string') {
+    return cls;
+  }
+  if (typeof cls === 'function') {
+    return cls(record, index, indent);
+  }
+  return '';
+}
