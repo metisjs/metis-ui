6f10987d7b9fc862a321ff6c1df71291e90b4309
王建明 <jianming.wang@dilu.com>
2025-01-17 10:47:07 +0800

    feat(drawer): 完善表单组件，移除未使用代码并优化布局

--- src/components/drawer/demo/form-in-drawer.tsx
+++ src/components/drawer/demo/form-in-drawer.tsx
@@ -1,145 +1,141 @@
-// TODO: form组件待开发
+import React, { useState } from 'react';
+import { PlusOutline } from '@metisjs/icons';
+import { Button, DatePicker, Drawer, Form, Input, Select } from 'metis-ui';
 
-import React from 'react';
+const App: React.FC = () => {
+  const [open, setOpen] = useState(false);
 
-export default () => {
-  return <div>form 待开发</div>;
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={showDrawer} icon={<PlusOutline />}>
+        New account
+      </Button>
+      <Drawer
+        title="Create a new account"
+        width={720}
+        onClose={onClose}
+        open={open}
+        footer={
+          <>
+            <Button onClick={onClose}>Cancel</Button>
+            <Button onClick={onClose} type="primary">
+              Submit
+            </Button>
+          </>
+        }
+      >
+        <Form layout="vertical" column={2} requiredMark={false}>
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[{ required: true, message: 'Please enter user name' }]}
+          >
+            <Input placeholder="Please enter user name" />
+          </Form.Item>
+          <Form.Item
+            name="url"
+            label="Url"
+            rules={[{ required: true, message: 'Please enter url' }]}
+          >
+            <Input
+              style={{ width: '100%' }}
+              addonBefore="http://"
+              addonAfter=".com"
+              placeholder="Please enter url"
+            />
+          </Form.Item>
+          <Form.Item
+            name="owner"
+            label="Owner"
+            rules={[{ required: true, message: 'Please select an owner' }]}
+          >
+            <Select
+              options={[
+                {
+                  label: 'Xiaoxiao Fu',
+                  value: 'xiao',
+                },
+                {
+                  label: 'Maomao Zhou',
+                  value: 'mao',
+                },
+              ]}
+              placeholder="Please select an owner"
+            ></Select>
+          </Form.Item>
+          <Form.Item
+            name="type"
+            label="Type"
+            rules={[{ required: true, message: 'Please choose the type' }]}
+          >
+            <Select
+              options={[
+                {
+                  label: 'Private',
+                  value: 'private',
+                },
+                {
+                  label: 'Public',
+                  value: 'public',
+                },
+              ]}
+              placeholder="Please choose the type"
+            ></Select>
+          </Form.Item>
+          <Form.Item
+            name="approver"
+            label="Approver"
+            rules={[{ required: true, message: 'Please choose the approver' }]}
+          >
+            <Select
+              options={[
+                {
+                  label: 'Jack Ma',
+                  value: 'jack',
+                },
+                {
+                  label: 'Tom Liu',
+                  value: 'tom',
+                },
+              ]}
+              placeholder="Please choose the approver"
+            ></Select>
+          </Form.Item>
+          <Form.Item
+            name="dateTime"
+            label="DateTime"
+            rules={[{ required: true, message: 'Please choose the dateTime' }]}
+          >
+            <DatePicker.RangePicker
+              style={{ width: '100%' }}
+              getPopupContainer={(trigger) => trigger.parentElement!}
+            />
+          </Form.Item>
+          <Form.Item
+            name="description"
+            label="Description"
+            rules={[
+              {
+                required: true,
+                message: 'please enter url description',
+              },
+            ]}
+            span={2}
+          >
+            <Input.TextArea rows={4} placeholder="please enter url description" />
+          </Form.Item>
+        </Form>
+      </Drawer>
+    </>
+  );
 };
 
-// import { PlusOutlined } from '@ant-design/icons';
-// import { Button, Col, DatePicker, Drawer, Form, Input, Row, Select } from 'metis-ui';
-// import React, { useState } from 'react';
-
-// const { Option } = Select;
-
-// const App: React.FC = () => {
-//   const [open, setOpen] = useState(false);
-
-//   const showDrawer = () => {
-//     setOpen(true);
-//   };
-
-//   const onClose = () => {
-//     setOpen(false);
-//   };
-
-//   return (
-//     <>
-//       <Button type="primary" onClick={showDrawer} icon={<PlusOutlined />}>
-//         New account
-//       </Button>
-//       <Drawer
-//         title="Create a new account"
-//         width={720}
-//         onClose={onClose}
-//         open={open}
-//         footer={
-//           <>
-//             <Button onClick={onClose}>Cancel</Button>
-//             <Button onClick={onClose} type="primary">
-//               Submit
-//             </Button>
-//           </>
-//         }
-//       >
-//         <Form layout="vertical" hideRequiredMark>
-//           <Row gutter={16}>
-//             <Col span={12}>
-//               <Form.Item
-//                 name="name"
-//                 label="Name"
-//                 rules={[{ required: true, message: 'Please enter user name' }]}
-//               >
-//                 <Input placeholder="Please enter user name" />
-//               </Form.Item>
-//             </Col>
-//             <Col span={12}>
-//               <Form.Item
-//                 name="url"
-//                 label="Url"
-//                 rules={[{ required: true, message: 'Please enter url' }]}
-//               >
-//                 <Input
-//                   style={{ width: '100%' }}
-//                   addonBefore="http://"
-//                   addonAfter=".com"
-//                   placeholder="Please enter url"
-//                 />
-//               </Form.Item>
-//             </Col>
-//           </Row>
-//           <Row gutter={16}>
-//             <Col span={12}>
-//               <Form.Item
-//                 name="owner"
-//                 label="Owner"
-//                 rules={[{ required: true, message: 'Please select an owner' }]}
-//               >
-//                 <Select placeholder="Please select an owner">
-//                   <Option value="xiao">Xiaoxiao Fu</Option>
-//                   <Option value="mao">Maomao Zhou</Option>
-//                 </Select>
-//               </Form.Item>
-//             </Col>
-//             <Col span={12}>
-//               <Form.Item
-//                 name="type"
-//                 label="Type"
-//                 rules={[{ required: true, message: 'Please choose the type' }]}
-//               >
-//                 <Select placeholder="Please choose the type">
-//                   <Option value="private">Private</Option>
-//                   <Option value="public">Public</Option>
-//                 </Select>
-//               </Form.Item>
-//             </Col>
-//           </Row>
-//           <Row gutter={16}>
-//             <Col span={12}>
-//               <Form.Item
-//                 name="approver"
-//                 label="Approver"
-//                 rules={[{ required: true, message: 'Please choose the approver' }]}
-//               >
-//                 <Select placeholder="Please choose the approver">
-//                   <Option value="jack">Jack Ma</Option>
-//                   <Option value="tom">Tom Liu</Option>
-//                 </Select>
-//               </Form.Item>
-//             </Col>
-//             <Col span={12}>
-//               <Form.Item
-//                 name="dateTime"
-//                 label="DateTime"
-//                 rules={[{ required: true, message: 'Please choose the dateTime' }]}
-//               >
-//                 <DatePicker.RangePicker
-//                   style={{ width: '100%' }}
-//                   getPopupContainer={(trigger) => trigger.parentElement!}
-//                 />
-//               </Form.Item>
-//             </Col>
-//           </Row>
-//           <Row gutter={16}>
-//             <Col span={24}>
-//               <Form.Item
-//                 name="description"
-//                 label="Description"
-//                 rules={[
-//                   {
-//                     required: true,
-//                     message: 'please enter url description',
-//                   },
-//                 ]}
-//               >
-//                 <Input.TextArea rows={4} placeholder="please enter url description" />
-//               </Form.Item>
-//             </Col>
-//           </Row>
-//         </Form>
-//       </Drawer>
-//     </>
-//   );
-// };
-
-// export default App;
+export default App;
