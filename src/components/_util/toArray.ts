c485627e3d73d7efc3f20d683963639a8818dd2f
王建明 <wangjianming.x@gmail.com>
2025-03-19 14:22:05 +0800

    fix(util): fixed toArray null value bug

--- src/components/_util/toArray.ts
+++ src/components/_util/toArray.ts
@@ -1,5 +1,5 @@
-export default function toArray<T>(candidate?: T | T[] | false): T[] {
-  if (candidate === undefined || candidate === false) return [];
+export default function toArray<T>(candidate?: T | T[] | false | null): T[] {
+  if (candidate === undefined || candidate === false || candidate === null) return [];
 
   return Array.isArray(candidate) ? candidate : [candidate];
 }
