4ee6612879cdf1eec5b64ec0a82da08b305792c0
王建明 <wangjianming.x@gmail.com>
2025-03-29 21:11:17 +0800

    chore: 更新打包配置

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,8 +3,13 @@ import { defineConfig } from 'father';
 export default defineConfig({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
   alias: { '@util/*': 'src/components/_util/*' },
-  esm: { output: 'es' },
-  cjs: { output: 'lib' },
+  esm: {
+    input: 'src',
+    output: 'dist',
+    platform: 'browser',
+    transformer: 'babel',
+    ignores: ['src/components/*/demo/*'],
+  },
   extraBabelPlugins: [
     [
       'import',
