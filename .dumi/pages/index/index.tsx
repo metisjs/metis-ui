0a9d97f79bea919078bd4286b342cb2a5c66f8ab
王建明 <wangjianming.x@gmail.com>
2025-04-13 21:52:46 +0800

    feat(site): homepage placeholder

--- .dumi/pages/index/index.tsx
+++ .dumi/pages/index/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Homepage: React.FC = () => {
-  return <section>Placeholder</section>;
+  return <section className="text-text mt-24 text-2xl">建设中...</section>;
 };
 
 export default Homepage;
