725990869f142f594afb8ac06e14e5811d748b21
WangJM001 <WangJM001@users.noreply.github.com>
2025-04-17 04:30:48 +0000

    deploy: ba59287e191dc0e1944d44d0dfa6aae53fdb969b

---
+++ collapse__demos.9a49ef7d.async.js
@@ -0,0 +1,37 @@
+"use strict";(self.webpackChunkmetis_ui=self.webpackChunkmetis_ui||[]).push([[6229],{20160:function(c,s,e){e.r(s);var h=e(39546),t=e(37365),a=e(14974),n=e(74132),i=`
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`,o=[{key:"1",label:"This is panel header 1",children:(0,n.jsx)("p",{children:i})},{key:"2",label:"This is panel header 2",children:(0,n.jsx)("p",{children:i})},{key:"3",label:"This is panel header 3",children:(0,n.jsx)("p",{children:i})}],l=function(){return(0,n.jsx)(a.default,{semantics:[{name:"root"},{name:"panel",children:[{name:"icon"},{name:"header"},{name:"content"}]}],children:(0,n.jsx)(t.Z,{items:o,defaultActiveKey:["1"]})})};s.default=l},52455:function(c,s,e){e.r(s);var h=e(39546),t=e(37365),a=e(74132),n=`
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`,i=[{key:"1",label:"This is panel header 1",children:(0,a.jsx)("p",{children:n})},{key:"2",label:"This is panel header 2",children:(0,a.jsx)("p",{children:n})},{key:"3",label:"This is panel header 3",children:(0,a.jsx)("p",{children:n})}],o=function(){return(0,a.jsx)(t.Z,{accordion:!0,items:i})};s.default=o},91840:function(c,s,e){e.r(s);var h=e(39546),t=e(37365),a=e(74132),n=`
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`,i=[{key:"1",label:"This is panel header 1",children:(0,a.jsx)("p",{children:n})},{key:"2",label:"This is panel header 2",children:(0,a.jsx)("p",{children:n})},{key:"3",label:"This is panel header 3",children:(0,a.jsx)("p",{children:n})}],o=function(){var d=function(u){console.log(u)};return(0,a.jsx)(t.Z,{items:i,defaultActiveKey:["1"],onChange:d})};s.default=o},58416:function(c,s,e){e.r(s);var h=e(39546),t=e(37365),a=e(74132),n=(0,a.jsx)("p",{className:"ps-6",children:"A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world."}),i=[{key:"1",label:"This is panel header 1",children:n},{key:"2",label:"This is panel header 2",children:n},{key:"3",label:"This is panel header 3",children:n}],o=function(){return(0,a.jsx)(t.Z,{items:i,bordered:!1,defaultActiveKey:["1"]})};s.default=o},9519:function(c,s,e){e.r(s);var h=e(39546),t=e(65257),a=e(37365),n=e(74132),i=`
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`,o=function(){return(0,n.jsxs)(t.Z,{vertical:!0,block:!0,children:[(0,n.jsx)(a.Z,{collapsible:"header",defaultActiveKey:["1"],items:[{key:"1",label:"This panel can only be collapsed by clicking text",children:(0,n.jsx)("p",{children:i})}]}),(0,n.jsx)(a.Z,{collapsible:"icon",defaultActiveKey:["1"],items:[{key:"1",label:"This panel can only be collapsed by clicking icon",children:(0,n.jsx)("p",{children:i})}]}),(0,n.jsx)(a.Z,{collapsible:"disabled",items:[{key:"1",label:"This panel can't be collapsed",children:(0,n.jsx)("p",{children:i})}]})]})};s.default=o},57177:function(c,s,e){e.r(s);var h=e(39546),t=e(6294),a=e(37365),n=e(74132),i=`
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`,o=function(r){return[{key:"1",label:"This is panel header 1",children:(0,n.jsx)("p",{children:i}),style:r},{key:"2",label:"This is panel header 2",children:(0,n.jsx)("p",{children:i}),style:r},{key:"3",label:"This is panel header 3",children:(0,n.jsx)("p",{children:i}),style:r}]},l=function(){var r={marginBottom:24,border:"none"};return(0,n.jsx)(a.Z,{bordered:!1,defaultActiveKey:["1"],expandIcon:function(_){var m=_.isActive;return m?(0,n.jsx)(t.PlusOutline,{}):(0,n.jsx)(t.MinusOutline,{})},items:o(r)})};s.default=l},96727:function(c,s,e){e.r(s);var h=e(48305),t=e.n(h),a=e(39546),n=e(6294),i=e(37365),o=e(53240),l=e(74132),d=`
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`,r=function(){var _=(0,a.useState)("start"),m=t()(_,2),v=m[0],p=m[1],P=function(E){p(E)},A=function(E){console.log(E)},f=function(){return(0,l.jsx)(n.Cog6ToothOutline,{className:"size-4 cursor-pointer",onClick:function(M){M.stopPropagation()}})},O=[{key:"1",label:"This is panel header 1",children:(0,l.jsx)("div",{children:d}),extra:f()},{key:"2",label:"This is panel header 2",children:(0,l.jsx)("div",{children:d}),extra:f()},{key:"3",label:"This is panel header 3",children:(0,l.jsx)("div",{children:d}),extra:f()}];return(0,l.jsxs)(l.Fragment,{children:[(0,l.jsx)(i.Z,{defaultActiveKey:["1"],onChange:A,expandIconPosition:v,items:O}),(0,l.jsx)("br",{}),(0,l.jsx)("span",{children:"Expand Icon Position: "}),(0,l.jsx)(o.ZP,{value:v,options:[{label:"start",value:"start"},{label:"end",value:"end"}],className:"ml-2 w-24",onChange:P})]})};s.default=r},55974:function(c,s,e){e.r(s);var h=e(39546),t=e(37365),a=e(74132),n=`
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`,i=[{key:"1",label:"This is panel header 1",children:(0,a.jsx)("p",{children:n})},{key:"2",label:"This is panel header 2",children:(0,a.jsx)("p",{children:n})},{key:"3",label:"This is panel header 3",children:(0,a.jsx)("p",{children:n})}],o=function(){return(0,a.jsx)(t.Z,{defaultActiveKey:["1"],ghost:!0,items:i})};s.default=o},55823:function(c,s,e){e.r(s);var h=e(39546),t=e(37365),a=e(74132),n=`
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`,i=[{key:"1",label:"This is panel nest panel",children:(0,a.jsx)("p",{children:n})}],o=[{key:"1",label:"This is panel header 1",children:(0,a.jsx)(t.Z,{defaultActiveKey:"1",items:i})},{key:"2",label:"This is panel header 2",children:(0,a.jsx)("p",{children:n})},{key:"3",label:"This is panel header 3",children:(0,a.jsx)("p",{children:n})}],l=function(){var r=function(_){console.log(_)};return(0,a.jsx)(t.Z,{onChange:r,items:o})};s.default=l},59449:function(c,s,e){e.r(s);var h=e(39546),t=e(37365),a=e(74132),n=`
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`,i=[{key:"1",label:"This is panel header with arrow icon",children:(0,a.jsx)("p",{children:n})},{key:"2",label:"This is panel header with no arrow icon",children:(0,a.jsx)("p",{children:n}),showArrow:!1}],o=function(){var d=function(u){console.log(u)};return(0,a.jsx)(t.Z,{defaultActiveKey:["1"],onChange:d,items:i})};s.default=o}}]);
